5bac5cedd62d5c645f1342d24401207939e02b3a
root <root@localhost.localdomain>
2021-01-07 10:26:17 +0530

    final commit

---
+++ android/app/src/main/kotlin/com/flutter/notifications/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.flutter.notifications
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/home.dart
@@ -0,0 +1,129 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'dart:async';
+
+import 'package:modal_progress_hud/modal_progress_hud.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'login.dart';
+import 'package:notifications/home.dart';
+import 'package:notifications/comment.dart';
+import 'package:notifications/navbar.dart';
+
+class FirestoreCRUDPage extends StatefulWidget {
+  @override
+  FirestoreCRUDPageState createState() {
+    return FirestoreCRUDPageState();
+  }
+}
+
+class FirestoreCRUDPageState extends State<FirestoreCRUDPage> {
+  // final FirebaseMessaging _messaging = FirebaseMessaging();
+  FirebaseMessaging _firebaseMessaging = new FirebaseMessaging();
+  FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+      new FlutterLocalNotificationsPlugin();
+
+  @override
+  void initState() {
+    super.initState();
+
+    var android = new AndroidInitializationSettings('mipmap/ic_launcher');
+    var ios = new IOSInitializationSettings();
+    var platform = new InitializationSettings(android, ios);
+    flutterLocalNotificationsPlugin.initialize(platform);
+
+    _firebaseMessaging.configure(
+      onMessage: (Map<String, dynamic> message) {
+        showNotification(message);
+        print('on message $message');
+      },
+      onResume: (Map<String, dynamic> message) {
+        print('on resume $message');
+      },
+      onLaunch: (Map<String, dynamic> message) {
+        print('on launch $message');
+      },
+    );
+    _firebaseMessaging.requestNotificationPermissions(
+        const IosNotificationSettings(sound: true, alert: true, badge: true));
+    _firebaseMessaging.onIosSettingsRegistered
+        .listen((IosNotificationSettings setting) {
+      print('IOS Setting Registed');
+    });
+    _firebaseMessaging.getToken().then((token) {
+      print(token);
+    });
+  }
+
+  showNotification(Map<String, dynamic> msg) async {
+    var android = new AndroidNotificationDetails(
+      'message',
+      "Title",
+      "Body",
+    );
+    var iOS = new IOSNotificationDetails();
+    var platform = new NotificationDetails(android, iOS);
+    await flutterLocalNotificationsPlugin.show(
+        0, "Donation App", "You have registered successfully", platform);
+  }
+
+  String id;
+  // ignore: deprecated_member_use
+  final db = Firestore.instance;
+  final _formKey = GlobalKey<FormState>();
+  String name;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Add Users'),
+      ),
+      body: ListView(
+        padding: EdgeInsets.all(8),
+        children: <Widget>[
+          Form(key: _formKey, child: buildTextFormField()),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              RaisedButton(
+                onPressed: createData,
+                child: Text('Create', style: TextStyle(color: Colors.white)),
+                color: Colors.green,
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  void createData() async {
+    if (_formKey.currentState.validate()) {
+      _formKey.currentState.save();
+      DocumentReference ref =
+          await db.collection('notifications').add({'name': '$name 😎'});
+      setState(() => id = ref.documentID);
+      print(ref.documentID);
+    }
+  }
+
+  TextFormField buildTextFormField() {
+    return TextFormField(
+      decoration: InputDecoration(
+        border: InputBorder.none,
+        hintText: 'name',
+        fillColor: Colors.grey[300],
+        filled: true,
+      ),
+      validator: (value) {
+        if (value.isEmpty) {
+          return 'Please enter some text';
+        }
+      },
+      onSaved: (value) => name = value,
+    );
+  }
+}
